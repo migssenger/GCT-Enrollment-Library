d65321e56afb0bdf28a24e7aa7f0dc74765b5f67
Miguel Garcia <miggysmart09@gmail.com>
2025-11-21 10:35:44 +0800

    Delete pages directory

--- pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  <style>co</style>
-  return (
-    <div><h1>About</h1></div>
-  )
-}
-export default About
--- pages/Visit.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Visit = () => {
-  return (
-    <h1>Visit</h1>
-  )
-}
-
-export default Visit
